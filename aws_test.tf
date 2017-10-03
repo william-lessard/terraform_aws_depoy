526b04b2e3e3346d32c0edfeed1fb170e5365f2e
William Lessard <williamdlessard@gmail.com>
2017-10-03 14:25:02 -0500

    adding new file

---
+++ aws_test.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+ 	region = "us-west-1"
+	profile = "default" 
+#	shared_credentials_file = "~/.aws/credentials"
+  access_key = "AKIAJE27XPIHADQOJPVQ"
+  secret_key = "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"
+}
+
+
+
+terraform {
+
+	required_version = "0.10.7"
+
+}
+
+
+resource "aws_instance" "test" {
+  ami           = "ami-3a674d5a"
+  instance_type = "t2.micro"
+  tags {
+	Name = "test_terraform-v1"
+  }
+
+}
+
+
+
+resource "aws_instance" "test1" {
+  ami           = "ami-3a674d5a"
+  instance_type = "t2.micro"
+  tags {
+        Name = "test_terraform-v2"
+  }
+
+}
+
